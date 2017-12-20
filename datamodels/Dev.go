0746208d50f21963ef857047980edd5bade65423
Matt S.Y. Ho <methodho@gmail.com>
2017-12-20 10:09:10 +0800

    bug fix

--- datamodels/Dev.go
+++ datamodels/Dev.go
@@ -4,13 +4,13 @@ import "strconv"
 
 type Dev struct {
 	Addr        string `json:"addr"`
-	Port        int    `json:"port"`
-	PublishPort int
+	Port        int    `json:"port"` // 最初傳進來的 port
+	PublishPort int    `json:"-"`    // 紀錄當前 publish 最後的 port
 }
 
 func (d *Dev) String() string {
-	if d.Port <= 0 {
+	if d.PublishPort <= 0 {
 		return d.Addr
 	}
-	return d.Addr + "/" + strconv.Itoa(d.Port)
+	return d.Addr + "/" + strconv.Itoa(d.PublishPort)
 }
