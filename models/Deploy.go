2a9da03e6385fc9f3013b17e4f76321c09d60e8c
Matt S.Y. Ho <methodho@gmail.com>
2018-01-31 17:16:31 +0800

    make CleanUp default

--- models/Deploy.go
+++ models/Deploy.go
@@ -36,6 +36,7 @@ func (d *Deploy) GroupContains(group string) bool {
 
 func NewDefaultDeploy() *Deploy {
 	return &Deploy{
+		CleanUp: true,
 		Dev: Dev{
 			IpAddress: "192.168.1.60",
 			Port:      0,
