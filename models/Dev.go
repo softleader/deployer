9dba81a5eb9ae96384ade7bc2942dc6b65628f27
Matt S.Y. Ho <methodho@gmail.com>
2018-07-13 17:22:39 +0800

    update

--- models/Dev.go
+++ models/Dev.go
@@ -9,7 +9,7 @@ type Dev struct {
 	IpAddress   string `json:"ipAddress,omitempty"`
 	Port        int    `json:"port,omitempty"` // 最初傳進來的 port
 	Ignore      string `json:"ignore,omitempty"`
-	PublishPort int    `json:"-"` // 紀錄當前 publish 最後的 port
+	PublishPort int    `json:"-" yaml:"-"` // 紀錄當前 publish 最後的 port
 }
 
 func (d *Dev) String() string {
